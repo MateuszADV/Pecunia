eb84938ab64541492db2969816481eac45360abb
MateuszADV <mateusz.adv@gmail.com>
2019-01-27 13:16:58 +0100

    Refaktor dodawania banknotów

--- src/main/java/pl/mateusz/Pecunia/controllers/viewController/NoteController.java
+++ src/main/java/pl/mateusz/Pecunia/controllers/viewController/NoteController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import pl.mateusz.Pecunia.models.Country;
 import pl.mateusz.Pecunia.models.Currency;
 import pl.mateusz.Pecunia.models.Note;
+import pl.mateusz.Pecunia.models.repositories.CountryRepository;
 import pl.mateusz.Pecunia.models.repositories.CurrencyRepository;
 import pl.mateusz.Pecunia.models.repositories.NoteRepository;
 import pl.mateusz.Pecunia.services.NoteService.NoteService;
+import pl.mateusz.Pecunia.services.countryService.CountryService;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,12 +23,14 @@ public class NoteController {
     private NoteService noteService;
     private NoteRepository noteRepository;
     private CurrencyRepository currencyRepository;
+    private CountryService countryService;
 
     @Autowired
-    public NoteController(NoteService noteService, NoteRepository noteRepository, CurrencyRepository currencyRepository) {
+    public NoteController(NoteService noteService, NoteRepository noteRepository, CurrencyRepository currencyRepository, CountryService countryService) {
         this.noteService = noteService;
         this.noteRepository = noteRepository;
         this.currencyRepository = currencyRepository;
+        this.countryService = countryService;
     }
 
     @GetMapping("/note")
@@ -64,8 +69,19 @@ public class NoteController {
     @GetMapping("/notes")
     public String getNotesSize(ModelMap modelMap) {
 
+        modelMap.addAttribute("countrys", countryService.countryDtoList().getCountryDtoList());
+
         List<Note> noteList = noteRepository.findAll();
         modelMap.addAttribute("noteList", noteList);
+
+        return "notes";
+    }
+
+    @GetMapping("/notes/{countryId}")
+    public String getNotesCountry(@PathVariable Long countryId, ModelMap modelMap) {
+        modelMap.addAttribute("currencyList", countryService.curencyFromCountryId(countryId));
+
+        System.out.println("powinno byćid państwa: " + countryId);
 
         return "notes";
     }
